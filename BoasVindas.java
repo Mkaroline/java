ed697095f94aa24857347986a02fd3d90a637c0e
maria karoline <mariakarolineminodemouram@gmail.com>
2023-07-02 00:28:53 -0300

    iniciando em java ehhhhh

---
+++ BoasVindas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BoasVindas{
+    public static void main (String[] args){
+    String nome;
+    Scanner entrada = new Scanner (System.in);
+
+    System.out.print("Informe seu nome: ");
+    nome = entrada.nextLine();
+
+    System.out.println("Bem Vindo(a): " + nome);
+
+    entrada.close();
+}
+
+}
