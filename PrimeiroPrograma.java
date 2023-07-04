06bac3bd99b8068f59294529b14a257b4502b4e5
maria karoline <mariakarolineminodemouram@gmail.com>
2023-07-04 14:51:00 -0300

    atualizandoo

--- PrimeiroPrograma.java
+++ PrimeiroPrograma.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BoasVindas{
+public class PrimeiroPrograma{
     public static void main (String[] args){
     String nome;
     Scanner entrada = new Scanner (System.in);
